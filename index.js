6e6bbc2aa9673ddc8d2c82fbb6135ab81ac2d94e
Vladislav Pogudin <bvlpo001@edu.xamk.fi>
2024-05-20 19:02:31 +0300

    socket added

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
 const app = express();
-const cors = require("cors");
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: ['https://notasurprise.netlify.app', 'http://localhost:3000'],
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+});
 
 app.use(cors({
-    origin: ['https://notasurprise.netlify.app', 'http://localhost:3000'],
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
+  origin: ['https://notasurprise.netlify.app', 'http://localhost:3000'],
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true
 }));
 
 let numbers = [1, 2, 3, 4, 5, 6, 7];
@@ -15,9 +26,10 @@ let startTime = Date.now();
 const updateCurrentNumber = () => {
   currentNumber = numbers[Math.floor(Math.random() * numbers.length)];
   startTime = Date.now();
+  io.emit('numberUpdated', { currentNumber, startTime });
 };
 
-setInterval(updateCurrentNumber, 90000);
+setInterval(updateCurrentNumber, 90000); // 1.5 minutes
 
 app.get("/get-current-number", (req, res) => {
   res.json({ currentNumber, startTime });
@@ -26,6 +38,7 @@ app.get("/get-current-number", (req, res) => {
 app.get("/numbers", (req, res) => {
   res.json({ "numbers": numbers });
 });
+
 
 app.get("/users", (req, res) => {
     res.json({ "users": ["userOne", "userTwo", "userThree", "userFour"] })
