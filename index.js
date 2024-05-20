38c25614b3e9d1f5feeb993e4b55a01e2a66e348
Vladislav Pogudin <bvlpo001@edu.xamk.fi>
2024-05-20 17:51:26 +0300

    try this

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require("cors");
 
+app.use(cors({
+    origin: ['https://notasurprise.netlify.app', 'http://localhost:3000'],
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+}));
 
 app.get("/users", (req, res) => {
     res.json({ "users": ["userOne", "userTwo", "userThree", "userFour"] })
