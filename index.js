062da6c27607684f6db39ce74cb72cd63a2c5654
Vladislav Pogudin <bvlpo001@edu.xamk.fi>
2024-05-20 15:42:03 +0300

    numbers added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/users", (req, res) => {
 })
 
 app.get("/numbers",  (req, res) => {
-    res.json({ "numbers": ["1", "2", "3", "4", "5"] })
+    res.json({ "numbers": [1, 2, 3, 4, 5, 6, 7] })
 })
 
 
