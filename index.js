e27f67d888c08f2550408b2456a06ea416d56007
Vladislav Pogudin <bvlpo001@edu.xamk.fi>
2025-02-19 16:36:13 +0200

    prettier

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const cors = require('cors');
-const path = require('path');
-const fs = require('fs'); // Подключаем модуль fs для работы с файлами
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: ['https://nikodle.netlify.app', 'http://localhost:3000'],
-    methods: ['GET', 'POST'],
-    credentials: true
-  }
+    origin: ["https://nikodle.netlify.app", "http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
 
-app.use(cors({
-  origin: ['https://nikodle.netlify.app', 'http://localhost:3000'],
-  methods: 'GET,POST,PUT,DELETE',
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["https://nikodle.netlify.app", "http://localhost:3000"],
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 
-const avatarsPath = path.join(__dirname, 'avatars'); //Avatars path
-app.use('/avatars', express.static(avatarsPath)); //Static files
+const avatarsPath = path.join(__dirname, "avatars"); //Avatars path
+app.use("/avatars", express.static(avatarsPath)); //Static files
 
-const chestsPath = path.join(__dirname, 'chests'); //Chests path
-app.use('/chests', express.static(chestsPath)); //Static files
+const chestsPath = path.join(__dirname, "chests"); //Chests path
+app.use("/chests", express.static(chestsPath)); //Static files
 
 let numbers = [1, 2, 3, 4, 5, 6, 7];
 let currentNumber = numbers[Math.floor(Math.random() * numbers.length)];
@@ -34,7 +36,7 @@ let startTime = Date.now();
 const updateCurrentNumber = () => {
   currentNumber = numbers[Math.floor(Math.random() * numbers.length)];
   startTime = Date.now();
-  io.emit('numberUpdated', { currentNumber, startTime });
+  io.emit("numberUpdated", { currentNumber, startTime });
 };
 
 setInterval(updateCurrentNumber, 90000); // 1.5 minutes
@@ -44,14 +46,14 @@ app.get("/get-current-number", (req, res) => {
 });
 
 app.get("/numbers", (req, res) => {
-  res.json({ "numbers": numbers });
+  res.json({ numbers: numbers });
 });
 
 app.get("/friends", (req, res) => {
-  fs.readFile('./friends.json', 'utf8', (err, data) => {
+  fs.readFile("./friends.json", "utf8", (err, data) => {
     if (err) {
-      console.error('Error reading friends.json:', err);
-      res.status(500).send('Internal Server Error');
+      console.error("Error reading friends.json:", err);
+      res.status(500).send("Internal Server Error");
     } else {
       const friends = JSON.parse(data);
       res.json(friends);
@@ -59,23 +61,22 @@ app.get("/friends", (req, res) => {
   });
 });
 
-
 app.get("/users", (req, res) => {
   res.json({
     users: [
       { name: "userOne", avatar: "userOne.jpg" },
       { name: "userTwo", avatar: "userTwo.jpg" },
       { name: "userThree", avatar: "userThree.jpg" },
-      { name: "userFour", avatar: "userFour.jpg" }
-    ]
+      { name: "userFour", avatar: "userFour.jpg" },
+    ],
   });
 });
 
 app.get("/champions", (req, res) => {
-  fs.readFile('./champions.json', 'utf8', (err, data) => {
+  fs.readFile("./champions.json", "utf8", (err, data) => {
     if (err) {
-      console.error('Error reading champions.json:', err);
-      res.status(500).send('Internal Server Error');
+      console.error("Error reading champions.json:", err);
+      res.status(500).send("Internal Server Error");
     } else {
       const champions = JSON.parse(data);
       res.json(champions);
@@ -84,10 +85,10 @@ app.get("/champions", (req, res) => {
 });
 
 app.get("/champions_lol", (req, res) => {
-  fs.readFile('./champions_lol.json', 'utf8', (err, data) => {
+  fs.readFile("./champions_lol.json", "utf8", (err, data) => {
     if (err) {
-      console.error('Error reading champions_lol.json:', err);
-      res.status(500).send('Internal Server Error');
+      console.error("Error reading champions_lol.json:", err);
+      res.status(500).send("Internal Server Error");
     } else {
       const champions_lol = JSON.parse(data);
       res.json(champions_lol);
